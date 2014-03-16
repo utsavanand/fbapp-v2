b9a0dcd4a0c71b4f935ae85f021a66297d99cf28
Utsav Anand <utsavanand.aggarwal@gmail.com>
2014-03-16 19:31:44 +0530

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 TangerineFbApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.assets.precompile = ['*.js', '*.css']
+  config.assets.precompile = ['*.js', '*.css','*.jpeg','*.png','*.gif','*.jpg']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
